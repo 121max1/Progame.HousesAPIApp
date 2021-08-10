54c4d754c3c8d36cbad73ed3d2aeab89517e6eb1
121max1 <x2w3g4@gmail.com>
2021-08-10 16:16:38 +0400

    отп

---
+++ Progame.HousesAPIApp.BLL.Interfaces/IOwnerService.cs
@@ -0,0 +1,18 @@
+﻿using Progame.HousesAPIApp.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Progame.HousesAPIApp.BLL.Interfaces
+{
+    public interface IOwnerService
+    {
+        void Add(Owner owner);
+
+        void Delete(Owner owner);
+
+        void GetAll();
+
+        void Update(Owner owner);
+    }
+}
